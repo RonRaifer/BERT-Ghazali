7b8ad06eabf18dce1122bf552c87a2c1aeac466e
Ron Raifer <ronraifer@gmail.com>
2021-03-09 14:33:07 +0200

    added adaptation to whole text

--- Main.py
+++ Main.py
@@ -6,17 +6,36 @@ pre_process = ArabertPreprocessor(model_name=model_name)
 model = AutoModel.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-text_original = "ومن ذلك التلحين في القرآن والآذان. وكذلك الاشتغال بدقائق الجدل والمناظرة من. "
-text_processed = pre_process.preprocess(text_original)
-print("Original Text:\n" + text_original)
-print("Processed Text:\n" + text_processed)
+t1_original_path = 'Data/Original/arab/t1/'
+t1_processed_path = 'Data/Processed/arab/t1/'
 
+myfile = open(t1_original_path + 'j1-k0.txt', mode="r", encoding="utf8")
+text_original = myfile.readlines()
+myfile.close()
+
+# text_original = "ومن ذلك التلحين في القرآن والآذان. وكذلك الاشتغال بدقائق الجدل والمناظرة من. "
+text_processed = []
+print("Original Text:\n")
+for line in text_original:
+    text_processed.append(pre_process.preprocess(line))
+    print(line)
+
+myfile = open(t1_processed_path + 'j1-k0.txt', mode="w", encoding="utf8")
+print("Processed Text:\n")
+for line in text_processed:
+    print(line)
+    myfile.write(line + '\n')
+
+myfile.close()
+
+text_processed_str = ' '.join(map(str, text_processed))
 # create tensor id's and tokenize the input
-inputs = tokenizer.encode_plus(text_processed, return_tensors='pt')
+inputs = tokenizer.encode_plus(text_processed_str, return_tensors='pt')
 print("Input ID's:")
 print(inputs['input_ids'][0])
 print(tokenizer.convert_ids_to_tokens(inputs['input_ids'][0]))
 
+'''
 outputs = model(**inputs)
 # Embedding with [CLS] and [SEP]
 emb = outputs['last_hidden_state']
@@ -36,3 +55,4 @@ print("Pooled vector output size:")
 print(pooled_vec.size())
 print("Pooled vector:")
 print(pooled_vec)
+'''
