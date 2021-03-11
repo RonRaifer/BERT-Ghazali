31860c46296d4a19b5b85ade606e2e6ba64ffdc9
Ron Raifer <ronraifer@gmail.com>
2021-03-11 13:29:13 +0200

    updated functionality of preprocessing all the text files

--- Main.py
+++ Main.py
@@ -1,39 +1,45 @@
+import os
+import glob
 from transformers import AutoTokenizer, AutoModel
 from preprocess import ArabertPreprocessor
+from pathlib import Path
 
 model_name = "aubmindlab/bert-base-arabertv2"
 pre_process = ArabertPreprocessor(model_name=model_name)
 model = AutoModel.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-t1_original_path = 'Data/Original/arab/t1/'
-t1_processed_path = 'Data/Processed/arab/t1/'
 
-myfile = open(t1_original_path + 'j1-k0.txt', mode="r", encoding="utf8")
-text_original = myfile.readlines()
-myfile.close()
+def start_preprocess(ts):
+    original_files = glob.glob('Data/Original/' + ts + '/*.txt')
+    processed_path = "Data/Processed/" + ts + "/"
+    for filename in original_files:
+        with open(filename, mode="r", encoding="utf8") as f:  # open in readonly mode
+            text_original = f.readlines()
+        processed_file = open(processed_path + Path(filename).stem + '.txt', mode="w", encoding="utf8")  # + '.txt'
+        for line_str in text_original:
+            processed_file.write(pre_process.preprocess(line_str) + '\n')
+        processed_file.close()
+
+
+start_preprocess('ts1')
+
+# myfile.close()
 
 # text_original = "ومن ذلك التلحين في القرآن والآذان. وكذلك الاشتغال بدقائق الجدل والمناظرة من. "
-text_processed = []
-print("Original Text:\n")
-for line in text_original:
-    text_processed.append(pre_process.preprocess(line))
-    print(line)
+# text_processed = []
+# print("Original Text:\n")
+# for line in text_original:
+#    text_processed.append(pre_process.preprocess(line))
+#    print(line)
 
-myfile = open(t1_processed_path + 'j1-k0.txt', mode="w", encoding="utf8")
-print("Processed Text:\n")
-for line in text_processed:
-    print(line)
-    myfile.write(line + '\n')
 
-myfile.close()
-
-text_processed_str = ' '.join(map(str, text_processed))
+# text_processed_str = ' '.join(map(str, text_processed))
 # create tensor id's and tokenize the input
-inputs = tokenizer.encode_plus(text_processed_str, return_tensors='pt')
-print("Input ID's:")
-print(inputs['input_ids'][0])
-print(tokenizer.convert_ids_to_tokens(inputs['input_ids'][0]))
+# inputs = tokenizer.encode_plus(text_processed_str, return_tensors='pt')
+# print("Input ID's:")
+# print(inputs['input_ids'][0])
+# print(tokenizer.convert_ids_to_tokens(inputs['input_ids'][0]))
 
 '''
 outputs = model(**inputs)
