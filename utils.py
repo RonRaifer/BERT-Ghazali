9d0f1ca26b30553cfba8d4b0835c4b6b8a8b91db
Ron Raifer <ronraifer@gmail.com>
2021-05-12 12:50:03 +0300

    changed UI to fit the new CNN

--- utils.py
+++ utils.py
@@ -9,19 +9,19 @@ params = {
     'F': 'minority',
 
     'KERNELS': 3,
-    # 'CNN_FILTERS': 500,
+    'CNN_FILTERS': 500,
     'LEARNING_RATE': 0.01,
     'NB_EPOCHS': 3,
     '1D_CONV_KERNEL': {1: 3, 2: 6, 3: 12},
-    # 'POOLING_SIZE': 500,
-    # 'DECAY': 1,
-    'OUTPUT_CLASSES': 1,
+    'POOLING_SIZE': 500,
+    'DECAY': 1,
+    'OUTPUT_CLASSES': 2,
     'STRIDES': 1,
     'BATCH_SIZE': 32,
-    # 'MOMENTUM': 0.9,
+    'MOMENTUM': 0.9,
     'ACTIVATION_FUNC': 'Relu',
 
-    # 'DNN_UNITS': 512,
+    'DNN_UNITS': 512,
     'DROPOUT_RATE': 0.2,
 }
 
@@ -34,16 +34,16 @@ silhouette_calc = None
 
 def LoadDefaultCNNConfig():
     params['KERNELS'] = 3
-    # params['CNN_FILTERS'] = 500
+    params['CNN_FILTERS'] = 500
     params['LEARNING_RATE'] = 0.01
     params['NB_EPOCHS'] = 10
     params['1D_CONV_KERNEL'] = {1: 3, 2: 6, 3: 12}
-    # params['POOLING_SIZE'] = 500
-    # params['DECAY'] = 1
+    params['POOLING_SIZE'] = 500
+    params['DECAY'] = 1
     params['OUTPUT_CLASSES'] = 1
     params['STRIDES'] = 1
     params['BATCH_SIZE'] = 32
-    # params['MOMENTUM'] = 0.9
+    params['MOMENTUM'] = 0.9
     params['ACTIVATION_FUNC'] = 'Relu'
     params['DROPOUT_RATE'] = 0.3
 
